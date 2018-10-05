251e2d0ec2ef27b4f3ff8a9586a0d74460d675fc
Matsumura Toshiaki <t.matsuwest.ern@gmail.com>
2018-10-05 11:13:42 +0900

    set up directories

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '@/stylus/entry.styl';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
@@ -8,3 +9,6 @@ new Vue({
   router,
   render: h => h(App),
 }).$mount('#app');
+
+__SIMPLE_API_ENDPOINT__ = "https://api.graph.cool/simple/v1/cjmsq7p9h9c1y0110ulbx5bnk"
+const networkInterface = createNetworkInterface({ uri: __SIMPLE_API_ENDPOINT__ })
